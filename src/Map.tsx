bd057ab4e4909a1efc0bcaca53a209aefcc706d3
Midkhat <midkhat@gmail.com>
2024-02-11 22:51:22 +1100

    GoogleMapReact

---
+++ src/Map.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { GoogleMap } from '@react-google-maps/api'
+
+const center = {
+  lat: -37.81,
+  lng: 144.96,
+}
+// const AnyReactComponent = ({ text }) => <div>{text}</div>
+
+export const Map = () => {
+  const defaultProps = {
+    center: center,
+    zoom: 11,
+  }
+
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      {/* <GoogleMap
+        id="map"
+        mapContainerStyle={{
+          margin: 'auto',
+          width: '100%',
+          height: '100%',
+        }}
+        zoom={15}
+        center={center}
+      ></GoogleMap> */}
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        {/* <AnyReactComponent text="My Marker" /> */}
+      </GoogleMapReact>
+    </div>
+  )
+}
+
+// {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         width: '100%',
+//         backgroundColor: 'gray',
+//       }}
+//     >
+//   <GoogleMap
+//     id="map"
+//     mapContainerStyle={{
+//       margin: 'auto',
+//       width: '100%',
+//       height: '100%',
+//     }}
+//     zoom={15}
+//     center={center}
+//   ></GoogleMap>
+//     </div>
+//   )
+// }
+
+// {
+//   /* <AnyReactComponent lat={-42.735258} lng={147.438} text="My Marker" /> */
+// }
---
+++ src/MapLoader.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { Map } from './Map'
+
+export const MapLoader: React.FC = () => {
+  const apiKey = import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY
+  const [apiLoaded, setApiLoaded] = useState(false)
+
+  useEffect(() => {
+    if (!apiKey) {
+      console.error('Google Maps API key is not provided.')
+      return
+    }
+
+    const script = document.createElement('script')
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&v=weekly`
+    script.async = true
+    script.defer = true
+    script.onload = () => {
+      console.log('Google Maps API loaded successfully')
+      setApiLoaded(true) // Set flag when API is loaded
+    }
+    script.onerror = () => {
+      console.error('Error loading Google Maps API')
+    }
+
+    document.head.appendChild(script)
+
+    return () => {
+      document.head.removeChild(script)
+    }
+  }, [apiKey])
+
+  return apiLoaded ? <Map /> : <p>loading</p> // Render Map component only if API is loaded
+}
